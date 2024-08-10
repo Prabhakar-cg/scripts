0fcb95892d6e67f2d8f6d2dc3619e78cd6a701a1
parvathy-subbiah <32271097+parvathy-subbiah@users.noreply.github.com>
2024-08-10 13:26:26 +0530

    NEW : pipelinejob

--- ansible.groovy
+++ ansible.groovy
@@ -2,3 +2,13 @@ folder('artemis') {
     displayName('Artemis')
     description('Artemis mission')
 }
+
+pipelineJob('Myfirstpipeline') {
+    definition {
+        cpsScm {
+            scm {
+                git('https://github.com/Prabhakar-cg/scripts.git')
+            }
+        }
+    }
+}
